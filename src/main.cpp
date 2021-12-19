6c4ac298fb34c0990f9e78c91f2fcfc6dd6147b4
Marius <maas0903@gmail.com>
2021-12-19 17:15:41 +0100

    Updates and minor stability improvements

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,11 @@
 #include <credentials.h>
 
 #include <Wire.h>
-#include <DHT.h>
 #include <SPI.h>
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
 #include <WiFiUdp.h>
 
@@ -14,13 +17,13 @@
 #include <ESP8266WebServer.h>
 
 //#define DEBUG
-IPAddress staticIP(192, 168, 63, 53);
+IPAddress staticIP(192, 168, 63, 65);
+String hostName = "kelder1";
 #define URI "/humid"
 IPAddress gateway(192, 168, 63, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress dns(192, 168, 63, 21);
 IPAddress dnsGoogle(8, 8, 8, 8);
-String hostName = "veranda";
 
 #define HTTP_REST_PORT 80
 #define WIFI_RETRY_DELAY 500
@@ -52,7 +55,113 @@ void BlinkNTimes(int pin, int blinks, unsigned long millies)
     }
 }
 
-int init_wifi()
+void get_temps()
+{
+    BlinkNTimes(LED_0, 2, 500);
+    StaticJsonDocument<1024> jsonObj;
+    deviceCount = 3;
+
+    try
+    {
+#ifdef DEBUG
+        jsonObj["DEBUG"] = "******* true *******";
+#else
+        jsonObj["DEBUG"] = "false";
+#endif
+        jsonObj["Hostname"] = hostName;
+        jsonObj["IpAddress"] = WiFi.localIP().toString();
+        jsonObj["MacAddress"] = WiFi.macAddress();
+        jsonObj["Gpio"] = 2;
+        jsonObj["DeviceType"] = "Humidity";
+        jsonObj["DeviceCount"] = deviceCount;
+
+        float th[2];
+#ifdef DEBUG
+        h = 55;
+        t = 22;
+#else
+        int numberOfTries = 0;
+        do
+        {
+            th[0] = dht.readTemperature();
+            delay(250);
+            th[1] = dht.readHumidity();
+            delay(250);
+            numberOfTries++;
+            Serial.print("Number of tries = ");
+            Serial.println(numberOfTries);
+            if (numberOfTries > 1)
+            {
+                dht.begin();
+                delay(500);
+            }
+
+        } while ((isnan(th[0]) || isnan(th[1])) && numberOfTries < 10);
+
+#endif
+
+        // Check if any reads failed and exit early (to try again).
+        if (numberOfTries == 10)
+        {
+            Serial.println("Failed to read from DHT sensor!");
+            deviceCount = 0;
+        }
+
+        if (deviceCount == 0)
+        {
+            Serial.print("No Content");
+            String sHostName(WiFi.hostname());
+            jsonObj["Error"] = "No devices found on " + sHostName + " (" + WiFi.macAddress() + ")";
+        }
+        else
+        {
+            jsonObj["numberOfTries"] = numberOfTries;
+            // Compute heat index in Celcius
+            float hic = dht.computeHeatIndex(th[0], th[1], false);
+
+            JsonArray sensors = jsonObj.createNestedArray("Sensors");
+            JsonObject sensor1 = sensors.createNestedObject();
+            sensor1["Id"] = "0";
+            sensor1["ValueType"] = "Humidity";
+            sensor1["Value"] = (String)th[1];
+            JsonObject sensor2 = sensors.createNestedObject();
+            sensor2["Id"] = "0";
+            sensor2["ValueType"] = "Temperature";
+            sensor2["Value"] = (String)th[0];
+            JsonObject sensor3 = sensors.createNestedObject();
+            sensor3["Id"] = "0";
+            sensor3["ValueType"] = "HeatIndex";
+            sensor3["Value"] = (String)hic;
+        }
+    }
+    catch (const std::exception &e)
+    {
+        // String exception = e.what();
+        // jsonObj["Exception"] = exception.substring(0, 99);
+        jsonObj["Exception"] = " ";
+        // std::cerr << e.what() << '\n';
+    }
+
+    String jSONmessageBuffer;
+    serializeJsonPretty(jsonObj, jSONmessageBuffer);
+
+    http_rest_server.sendHeader("Access-Control-Allow-Origin", "*");
+    http_rest_server.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    http_rest_server.send(200, "application/json", jSONmessageBuffer);
+
+    Serial.println(jSONmessageBuffer);
+}
+
+void config_rest_server_routing()
+{
+    http_rest_server.on("/", HTTP_GET, []()
+                        { http_rest_server.send(200, "text/html",
+                                                "Welcome to the ESP8266 REST Web Server: " + hostName); });
+    http_rest_server.on(URI, HTTP_GET, get_temps);
+}
+
+void init_wifi()
 {
     int retries = 0;
 
@@ -70,128 +179,43 @@ int init_wifi()
         Serial.print("#");
     }
     Serial.println();
-    BlinkNTimes(LED_0, 3, 500);
-    return WiFi.status();
-}
 
-void get_temps()
-{
-    BlinkNTimes(LED_0, 2, 500);
-    StaticJsonBuffer<600> jsonBuffer;
-    JsonObject &jsonObj = jsonBuffer.createObject();
-    char JSONmessageBuffer[600];
-    deviceCount = 3;
-
-    try
-    {
-#ifdef DEBUG
-        jsonObj["DEBUG"] = "******* true *******";
-#else
-        jsonObj["DEBUG"] = "false";
-#endif
-        jsonObj["Hostname"] = hostName;
-        jsonObj["IpAddress"] = WiFi.localIP().toString();
-        jsonObj["MacAddress"] = WiFi.macAddress();
-        jsonObj["Gpio"] = 2;
-        jsonObj["DeviceType"] = "Humidity";
-        jsonObj["DeviceCount"] = deviceCount;
-
-        float h;
-        float t;
-#ifdef DEBUG
-        h = 55;
-        t = 22;
-#else
-        h = dht.readHumidity();
-        t = dht.readTemperature();
-#endif
-
-        String mess;
-        // Check if any reads failed and exit early (to try again).
-        if (isnan(h) || isnan(t))
-        {
-            mess = "Failed to read from DHT sensor!";
-            Serial.println(mess);
-            deviceCount = 0;
-        }
-
-        if (deviceCount == 0)
-        {
-            Serial.print("No Content");
-            String sHostName(WiFi.hostname());
-            jsonObj["Error"] = "No devices found on " + sHostName + " (" + WiFi.macAddress() + ")";
-        }
-        else
-        {
-            // Compute heat index in Celcius
-            float hic = dht.computeHeatIndex(t, h, false);
- 
-            JsonArray &sensors = jsonObj.createNestedArray("Sensors");
-            JsonObject &sensor1 = sensors.createNestedObject();
-            sensor1["Id"] = "0";
-            sensor1["ValueType"] = "Humidity";
-            sensor1["Value"] = (String)h;
-            JsonObject &sensor2 = sensors.createNestedObject();
-            sensor2["Id"] = "0";
-            sensor2["ValueType"] = "Temperature";
-            sensor2["Value"] = (String)t;
-            JsonObject &sensor3 = sensors.createNestedObject();
-            sensor3["Id"] = "0";
-            sensor3["ValueType"] = "HeatIndex";
-            sensor3["Value"] = (String)hic;
-        }
-    }
-    catch (const std::exception &e)
-    {
-        // String exception = e.what();
-        // jsonObj["Exception"] = exception.substring(0, 99);
-        jsonObj["Exception"] = " ";
-        //std::cerr << e.what() << '\n';
-    }
-
-    jsonObj.prettyPrintTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-
-    http_rest_server.sendHeader("Access-Control-Allow-Origin", "*");
-    http_rest_server.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-    http_rest_server.send(200, "application/json", JSONmessageBuffer);
-
-    Serial.println(JSONmessageBuffer);
-}
-
-void config_rest_server_routing()
-{
-    http_rest_server.on("/", HTTP_GET, []() {
-        http_rest_server.send(200, "text/html",
-                              "Welcome to the ESP8266 REST Web Server: " + hostName);
-    });
-    http_rest_server.on(URI, HTTP_GET, get_temps);
-}
-
-void setup(void)
-{
-    Serial.begin(115200);
-
-    pinMode(DHTPIN, INPUT);
-    dht.begin();
-
-    if (init_wifi() == WL_CONNECTED)
+    if (WiFi.status() == WL_CONNECTED)
     {
         Serial.print("Connected to ");
         Serial.print(ssid);
         Serial.print("--- IP: ");
         Serial.println(WiFi.localIP());
+        BlinkNTimes(LED_0, 3, 500);
     }
     else
     {
         Serial.print("Error connecting to: ");
         Serial.println(ssid);
     }
+}
+
+void resetInit()
+{
+    pinMode(DHTPIN, INPUT);
+    delay(200);
+    dht.begin();
+    delay(200);
+
+    init_wifi();
+    delay(200);
 
     config_rest_server_routing();
-
+    delay(200);
     http_rest_server.begin();
+
     Serial.println("HTTP REST Server Started");
+}
+
+void setup(void)
+{
+    Serial.begin(115200);
+    resetInit();
 }
 
 void loop(void)
