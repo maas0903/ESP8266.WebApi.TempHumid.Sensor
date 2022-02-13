570150c17f7430f94e421d14ae810d81362a2d5f
Marius <maas0903@gmail.com>
2022-02-13 15:37:47 +0100

    added influxdb functionality for esp-12f

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,15 @@
 #include <stdio.h>
-#include <ArduinoJson.h>
 #include <Arduino.h>
-
 #include <Wire.h>
 #include <SPI.h>
-
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
-
-#include <WiFiUdp.h>
-
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-
 #include <credentials.h>
 #include <OTA.h>
-
 #include <SSD1306Wire.h>
+#include <InfluxDbClient.h>
 
 SSD1306Wire display(0x3c, D2, D1); // D2=SDK  D1=SCK  As per labeling on NodeMCU
 
@@ -25,25 +17,25 @@ SSD1306Wire display(0x3c, D2, D1); // D2=SDK  D1=SCK  As per labeling on NodeMCU
 #define LED_0 0
 #define DHTPIN 2
 #define URI "/humid"
-
-#define HTTP_REST_PORT 80
 #define WIFI_RETRY_DELAY 500
 #define MAX_WIFI_INIT_RETRY 50
-
 #define DHTTYPE DHT11 // DHT22 (AM2302), AM2321
+
+#define INFLUXDB_URL "http://192.168.63.28:8086"
+#define INFLUXDB_TOKEN "tokedid"
+#define INFLUXDB_ORG "org"
+#define INFLUXDB_BUCKET "sensors"
+
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+Point sensor("room_sensor");
+unsigned long previousMillisWiFi = 0;
 
 DHT dht(DHTPIN, DHTTYPE);
 
 unsigned long time_now = 0;
 int period = 1 * 60 * 1000; // 120000;
 
-// Define NTP Client to get time
-WiFiUDP ntpUDP;
-const long utcOffsetInSeconds = 0;
-
 int deviceCount = 3;
-
-ESP8266WebServer http_rest_server(HTTP_REST_PORT);
 
 void BlinkNTimes(int pin, int blinks, unsigned long millies)
 {
@@ -91,24 +83,10 @@ void get_temps()
     reading = true;
     displayStatus("Spaarkamer - Lees ....", true);
     BlinkNTimes(LED_0, 2, 500);
-    StaticJsonDocument<1024> jsonObj;
     deviceCount = 3;
 
     try
     {
-#ifdef DEBUG
-        jsonObj["DEBUG"] = "******* true *******";
-#else
-        jsonObj["DEBUG"] = "false";
-#endif
-        jsonObj["Hostname"] = hostName;
-        jsonObj["IpAddress"] = WiFi.localIP().toString();
-        jsonObj["MacAddress"] = WiFi.macAddress();
-        jsonObj["Gpio"] = 2;
-        jsonObj["DeviceType"] = "Humidity";
-        jsonObj["DeviceCount"] = deviceCount;
-
-        // float th[2];
 #ifdef DEBUG
         h = 55;
         t = 22;
@@ -163,61 +141,35 @@ void get_temps()
         {
             Serial.print("No Content");
             String sHostName(WiFi.hostname());
-            jsonObj["Error"] = "No devices found on " + sHostName + " (" + WiFi.macAddress() + ")";
         }
         else
         {
             th[0] = th[0] / numberRead;
             th[1] = th[1] / numberRead;
-            jsonObj["numberOfTries"] = numberOfTries;
-            jsonObj["numberOfRead"] = numberRead;
             // Compute heat index in Celcius
             float hic = dht.computeHeatIndex(th[0], th[1], false);
 
-            JsonArray sensors = jsonObj.createNestedArray("Sensors");
-            JsonObject sensor1 = sensors.createNestedObject();
-            sensor1["Id"] = "0";
-            sensor1["ValueType"] = "Humidity";
-            sensor1["Value"] = (String)th[1];
-            JsonObject sensor2 = sensors.createNestedObject();
-            sensor2["Id"] = "0";
-            sensor2["ValueType"] = "Temperature";
-            sensor2["Value"] = (String)th[0];
-            JsonObject sensor3 = sensors.createNestedObject();
-            sensor3["Id"] = "0";
-            sensor3["ValueType"] = "HeatIndex";
-            sensor3["Value"] = (String)hic;
+            sensor.clearFields();
+
+            sensor.addField("temperature", th[0]);
+            sensor.addField("humidity", th[1]);
+            sensor.addField("heat-index", hic);
+            Serial.print("Writing: ");
+            Serial.println(client.pointToLineProtocol(sensor));
+
+            if (!client.writePoint(sensor))
+            {
+                Serial.print("InfluxDB write failed: ");
+                Serial.println(client.getLastErrorMessage());
+            }
         }
     }
     catch (const std::exception &e)
     {
-        // String exception = e.what();
-        // jsonObj["Exception"] = exception.substring(0, 99);
-        jsonObj["Exception"] = " ";
-        // std::cerr << e.what() << '\n';
     }
-
-    String jSONmessageBuffer;
-    serializeJsonPretty(jsonObj, jSONmessageBuffer);
-
-    http_rest_server.sendHeader("Access-Control-Allow-Origin", "*");
-    http_rest_server.sendHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    http_rest_server.sendHeader("Refresh", "30"); 
-
-    http_rest_server.send(200, "application/json", jSONmessageBuffer);
-
-    Serial.println(jSONmessageBuffer);
 
     displayStatus("Spaarkamer");
     reading = false;
-}
-
-void config_rest_server_routing()
-{
-    http_rest_server.on("/", HTTP_GET, []()
-                        { http_rest_server.send(200, "text/html",
-                                                "Welcome to the ESP8266 REST Web Server: " + hostName); });
-    http_rest_server.on(URI, HTTP_GET, get_temps);
 }
 
 boolean init_wifi()
@@ -277,16 +229,23 @@ void resetInit()
 
     setupOTA("Spaarkamer", ssid, password);
 
-    config_rest_server_routing();
-    delay(200);
-    http_rest_server.begin();
-
     Serial.println("HTTP REST Server Started");
     displayStatus("HTTP REST Server Started", true);
 
     delay(1000);
 
-    get_temps();
+    if (client.validateConnection())
+    {
+        Serial.print("Connected to InfluxDB: ");
+        Serial.println(client.getServerUrl());
+    }
+    else
+    {
+        Serial.print("InfluxDB connection failed: ");
+        Serial.println(client.getLastErrorMessage());
+
+        get_temps();
+    }
 }
 
 void setup(void)
@@ -302,15 +261,12 @@ void loop(void)
 
     delay(200);
 
-    /*if (millis() > time_now + period)
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillisWiFi >= 15 * 1000) // send data every 15 sec
     {
-        if (!reading)
-        {
-            get_temps();
-        }
-        time_now = millis();
+        get_temps();
+        previousMillisWiFi = currentMillis;
+        Serial.print(F("Wifi is still connected with IP: "));
+        Serial.println(WiFi.localIP()); // inform user about his IP address
     }
-    */
-
-    http_rest_server.handleClient();
 }
