49626ea194e04d29b8d463ae05d676feef766e6e
Marius <maas0903@gmail.com>
2022-04-02 23:55:11 +0200

    added influxdb support

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,14 @@
 #include <DHT_U.h>
 #include <ESP8266WiFi.h>
 #include <credentials.h>
-#include <OTA.h>
 #include <InfluxDbClient.h>
 
 //#define DEBUG
 #define LED_0 0
 #define DHTPIN 2
-#define URI "/humid"
 #define WIFI_RETRY_DELAY 500
 #define MAX_WIFI_INIT_RETRY 50
-#define DHTTYPE DHT22 // DHT11 (AM2302), AM2321
+#define DHTTYPE DHT22 // DHT11
 
 #define INFLUXDB_URL "http://192.168.63.28:8086"
 #define INFLUXDB_TOKEN "tokedid"
@@ -24,7 +22,7 @@
 #define INFLUXDB_BUCKET "sensors"
 
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
-Point sensor("tent");
+Point sensor("humid-test2");
 unsigned long previousMillisWiFi = 0;
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -33,6 +31,8 @@ unsigned long time_now = 0;
 int period = 1 * 60 * 1000; // 120000;
 
 int deviceCount = 3;
+
+int resets = 0;
 
 void BlinkNTimes(int pin, int blinks, unsigned long millies)
 {
@@ -47,99 +47,6 @@ void BlinkNTimes(int pin, int blinks, unsigned long millies)
 }
 
 float th[2];
-
-boolean reading = false;
-
-void get_temps()
-{
-    reading = true;
-    BlinkNTimes(LED_0, 2, 500);
-    deviceCount = 3;
-
-    try
-    {
-#ifdef DEBUG
-        h = 55;
-        t = 22;
-#else
-        int numberOfTries = 0;
-        int numberRead = 0;
-        int numberNotRead = 0;
-        float temperature = 0;
-        float humidity = 0;
-        do
-        {
-            temperature = dht.readTemperature();
-            delay(250);
-            humidity = dht.readHumidity();
-            delay(250);
-            numberOfTries++;
-
-            if (isnan(temperature) || isnan(humidity))
-            {
-                numberNotRead++;
-            }
-            else
-            {
-                numberRead++;
-                th[0] = th[0] + temperature;
-                th[1] = th[1] + humidity;
-            }
-
-            Serial.print("Number of tries = ");
-            Serial.println(numberOfTries);
-
-            if (numberNotRead > 1)
-            {
-                dht.begin();
-                delay(500);
-                numberNotRead = 0;
-            }
-
-        } while (numberOfTries < 20);
-
-#endif
-
-        // Check if any reads failed and exit early (to try again).
-        if (numberRead == 0)
-        {
-            Serial.println("Failed to read from DHT sensor!");
-            deviceCount = 0;
-        }
-
-        if (deviceCount == 0)
-        {
-            Serial.print("No Content");
-            String sHostName(WiFi.hostname());
-        }
-        else
-        {
-            th[0] = th[0] / numberRead;
-            th[1] = th[1] / numberRead;
-            // Compute heat index in Celcius
-            float hic = dht.computeHeatIndex(th[0], th[1], false);
-
-            sensor.clearFields();
-
-            sensor.addField("temperature", th[0]);
-            sensor.addField("humidity", th[1]);
-            sensor.addField("heat-index", hic);
-            Serial.print("Writing: ");
-            Serial.println(client.pointToLineProtocol(sensor));
-
-            if (!client.writePoint(sensor))
-            {
-                Serial.print("InfluxDB write failed: ");
-                Serial.println(client.getLastErrorMessage());
-            }
-        }
-    }
-    catch (const std::exception &e)
-    {
-    }
-
-    reading = false;
-}
 
 boolean init_wifi()
 {
@@ -178,6 +85,8 @@ boolean init_wifi()
 
 void resetInit()
 {
+    resets++;
+    Serial.begin(115200);
     pinMode(DHTPIN, INPUT);
     delay(200);
     dht.begin();
@@ -185,16 +94,10 @@ void resetInit()
 
     if (!init_wifi())
     {
-        ESP.restart;
+        ESP.restart();
     }
 
     delay(200);
-
-    setupOTA("Spaarkamer", ssid, password);
-
-    Serial.println("HTTP REST Server Started");
-
-    delay(1000);
 
     if (client.validateConnection())
     {
@@ -205,30 +108,121 @@ void resetInit()
     {
         Serial.print("InfluxDB connection failed: ");
         Serial.println(client.getLastErrorMessage());
+    }
+}
 
-        get_temps();
+void get_temps()
+{
+    BlinkNTimes(LED_0, 2, 500);
+    deviceCount = 3;
+
+    try
+    {
+#ifdef DEBUG
+        h = 55;
+        t = 22;
+#else
+        int numberOfTries = 0;
+        int numberRead = 0;
+        int numberNotRead = 0;
+        float temperature = 0;
+        float humidity = 0;
+        do
+        {
+            temperature = dht.readTemperature();
+            delay(1000);
+            humidity = dht.readHumidity();
+            delay(1000);
+            numberOfTries++;
+
+            if (isnan(temperature) || isnan(humidity))
+            {
+                numberNotRead++;
+            }
+            else
+            {
+                numberRead++;
+                th[0] = th[0] + temperature;
+                th[1] = th[1] + humidity;
+            }
+
+            Serial.print("Number of tries = ");
+            Serial.println(numberOfTries);
+
+            if (numberNotRead > 6)
+            {
+                Serial.println("Beginning DHT again");
+                dht.begin();
+                delay(1000);
+                numberNotRead = 0;
+            }
+
+        } while (numberOfTries < 20);
+
+#endif
+
+        if (numberRead == 0)
+        {
+            Serial.println("Failed to read from DHT sensor!");
+            deviceCount = 0;
+        }
+
+        if (deviceCount == 0)
+        {
+            Serial.print("No Content");
+            resetInit();
+        }
+        else
+        {
+            th[0] = th[0] / numberRead;
+            th[1] = th[1] / numberRead;
+
+            float hic = dht.computeHeatIndex(th[0], th[1], false);
+
+            sensor.clearFields();
+
+            sensor.addField("temperature", th[0]);
+            sensor.addField("humidity", th[1]);
+            sensor.addField("heat-index", hic);
+            sensor.addField("resets", resets);
+            sensor.addField("ipaddress", WiFi.localIP().toString());
+            sensor.addField("mac-address", WiFi.macAddress());
+            Serial.print("writing: ");
+            Serial.println(client.pointToLineProtocol(sensor));
+
+            while (!client.canSendRequest())
+            {
+                Serial.println("client not ready");
+            }
+
+            if (!client.writePoint(sensor))
+            {
+                Serial.print("InfluxDB write failed: ");
+                Serial.println(client.getLastErrorMessage());
+                resetInit();
+            }
+            Serial.println("done");
+        }
+    }
+    catch (const std::exception &e)
+    {
     }
 }
 
 void setup(void)
 {
-    Serial.begin(115200);
     resetInit();
+    get_temps();
 }
 
 void loop(void)
 {
-    // if (!reading)
-    ArduinoOTA.handle();
-
-    delay(200);
-
     unsigned long currentMillis = millis();
-    if (currentMillis - previousMillisWiFi >= 15 * 1000) // send data every 15 sec
+    if (currentMillis - previousMillisWiFi >= 120 * 1000)
     {
         get_temps();
         previousMillisWiFi = currentMillis;
         Serial.print(F("Wifi is still connected with IP: "));
-        Serial.println(WiFi.localIP()); // inform user about his IP address
+        Serial.println(WiFi.localIP());
     }
 }
